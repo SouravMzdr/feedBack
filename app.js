f5b4d227908145d271712dbf19954bb1dc54dbf5
rurugit <neog.anupal101@gmail.com>
2019-02-02 21:22:35 +0530

    Create teacher index page and display all teachers

--- app.js
+++ app.js
@@ -1,19 +1,76 @@
-var express = require("express")
-	app		= express();	
+var express 	= require("express")
+	app			= express(),
+	bodyParser 	= require("body-parser");	
+
+var teacher =[
+	{
+		name : "Himanish",
+		// subject : [
+		// {
+		// 	subject  : "Graph Theory",
+		// 	subCode  : "GP101",
+		// 	semester : 4
+		// },
+		// {
+		// 	subject  : "DBMS",
+		// 	subCode  : "DM101",
+		// 	semester : 5 
+		// }
+
+		// ]
+		// branch : "cse",
+		image  : "/images/default.png"
+
+
+	},
+	{
+	name : "Diganta",
+	// subject : [
+	// {
+	// 	subject  : "DSA",
+	// 	subCode  : "DB102",
+	// 	semester : 4
+	// },
+	// {
+	// 	subject  : "TOC",
+	// 	subCode  : "TC101",
+	// 	semester : 5 
+	// }
+
+	// ]
+	// branch : "cse",
+	image  : "/images/default.png"
+	}
+];
 
 
 
-app.use("view engone", ejs);
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 //main route
 app.get("/", (req, res) =>{
 	res.render("main");
 });	
 
-//
+//admin route
+app.get("/admin", (req, res) =>{
+	res.render("admin")
+});
 
+//user route
+app.get("/admin", (req, res) =>{
+	res.send("will be implemented later");
+});
 
+//show all teacher route 
+//index
+app.get("/teacher", (req, res) => {
+	res.render("teacher", {teacher : teacher});
+});
+
+//show
 
 
 
